ccc5afffa9275d39c75f3ee7b59094956affa810
Matthew Munger <matthew.munger@outlook.com>
2015-12-08 15:42:49 -0400

    Added director /Completed

---
+++ 31.py
@@ -0,0 +1,24 @@
+__author__ = 'Matthew'
+
+currencies_default = [1, 2, 5, 10, 20, 50, 100, 200]  # 1p, 2p, 5p, 10p, 20p, 50p, 1E, 2E
+currencies_reverse = currencies_default.reverse()
+two_euros = 200
+pieces = currencies_default
+combos = []
+
+
+def add_piece(piece, total):
+    total += piece
+
+
+def check(total):
+    if total == 200:
+        return 1
+    elif total > 200:
+        return 2
+    else:
+        return 0
+
+def recursion(depth):
+    for piece in currencies_reverse:
+        # Trued recursion, failed badly
--- scripts.py
+++ scripts.py
@@ -1,10 +1,8 @@
-def factorial(n):
-    final = n
-    r = n - 1
-    while r > 0:
-        final *= r
-        r -= 1
-    return final
+def factorial(num):
+    if num == 1:
+        return True
+    else:
+        return num * factorial(num - 1)
 
 
 def isPrime(num):
