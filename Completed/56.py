4a73e7cf9f0ea61f4c84d33a4ef432522c9770f2
Matthew Munger <matthew.munger@outlook.com>
2015-12-20 07:47:35 -0400

    Added 56, removed unnecessary files.

---
+++ Completed/56.py
@@ -0,0 +1,17 @@
+largest = [0, 0, 0, 0]
+total = 0
+
+for a in range(1, 100):
+    print("At A = %s" % a)
+    for b in range(1, 100):
+        count = 1
+        total = 0
+        for c in range(1, b):
+            count *= a
+        for char in str(count):
+            total += int(char)
+        if total > largest[3]:
+            largest = count, a, b, total
+
+print("Largest number: A = %s, B = %s, Number = %s" % (largest[1], largest[2], largest[0]))
+print("Answer: %s" % largest[3])
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # ProjectEulerPython
-A repositary that holds all my finished project euler scripts.
+A repository that holds all my finished project euler scripts.
 The answers to the problems are in the last line of each file.
