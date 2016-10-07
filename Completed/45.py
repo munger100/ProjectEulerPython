e97aeda1600406fa9088ed08ba5c1f84508846eb
Munger <matthew.munger@gmail.com>
2016-10-07 14:21:29 -0400

    Completed 45

---
+++ Completed/45.py
@@ -0,0 +1,40 @@
+import sys
+
+
+def triangle(num):
+    tri = num * (num + 1) / 2
+    return int(tri)
+
+
+def pentagon(num):
+    penta = (num * (3 * num - 1)) / 2
+    return int(penta)
+
+
+def hexagon(num):
+    hexa = num * (2 * num - 1)
+    return int(hexa)
+
+
+count = 0
+pents = []
+hexs = []
+i = 0
+while count < 2:
+    if i % 1000 == 0:
+        print("At %s" % i)
+    tri = triangle(i)
+    if tri in pents and tri in hexs:
+        count += 1
+        print("Found %s in each" % tri)
+        # if count == 2:
+        #     sys.exit(tri)
+    pents.append(pentagon(i))
+    hexs.append(hexagon(i))
+    i += 1
+
+print("Answer: %s" % tri)
+
+# print(triangle(285), pentagon(165), hexagon(143))  # should = 40755 each
+
+# Answer: 1533776805
