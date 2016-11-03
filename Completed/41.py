004458ce51046c527bb9441c63bf2ae4eabde3bb
Munger <matthew.munger@gmail.com>
2016-11-03 14:49:59 -0400

    Complete 41

---
+++ Completed/41.py
@@ -0,0 +1,24 @@
+from Sources.Scripts import is_prime
+
+
+def is_pandigital(n):
+    n = str(n)
+    for i in range(1, len(n) + 1):
+        if str(i) not in n:
+            return False
+    return True
+
+
+highest = 0
+
+limit = 999999999
+
+for i in reversed(range(1, limit, 2)):
+    if i % 10000001 == 0:
+        print("At %s" % i)
+    if is_pandigital(i):
+        if is_prime(i):
+            print("%s is largest pandigital prime" % i)
+            break
+
+# Answer: 7652413
--- test.py
+++ test.py
@@ -18,8 +18,18 @@
 #
 # print(arr)
 
-a = "asdasds"
-b = list(a)
-print(a)
-print(b)
-print(''.join(b))
+# a = "asdasds"
+# b = list(a)
+# print(a)
+# print(b)
+# print(''.join(b))
+
+
+def is_pandigital(n):
+    n = str(n)
+    for i in range(1, len(n) + 1):
+        if str(i) not in n:
+            return False
+    return True
+
+print(is_pandigital(999999999))
