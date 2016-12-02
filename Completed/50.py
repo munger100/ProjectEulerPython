bed2d0c08a87176fe2ad497fa689c7a4cd40390a
Inconceivable <mattfromthecountry@hotmail.com>
2016-12-02 15:39:14 -0500

    Completed 50

--- Completed/50.py
+++ Completed/50.py
@@ -6,25 +6,18 @@ highest = [0.0]
 
 
 def find_consecs(n, p, h):
-    arr = []
     for i in range(0, len(p)):
-        c = 0
-        j = 0
+        c, j = 0, 0
+
         while c < n:
             c += p[i + j]
             j += 1
-            # print("c = %s" % c)
             if c == n:
-                # print("Yay %s, %s" % (n, j))
                 if j > h[0]:
                     h = [j, n]
-                arr.append([n, j])
             if c > n:
                 break
     return h
-
-    # WAT
-
 
 for i in range(1, limit):
     if i % 10000 == 0:
